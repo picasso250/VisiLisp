10f12923063ff4f0ae1f6533536d80091e74be9f
pica <xi_aochi@163.com>
2024-05-12 20:56:59 +0800

    cond

--- visilisp.js
+++ visilisp.js
@@ -25,13 +25,21 @@ function evaluateExpression(expression) {
             } else {
                 throw new Error('Division operator expects 2 arguments');
             }
+        case 'cond':
+            for (let i = 0; i < args.length; i += 2) {
+                if (evaluateExpression(args[i])) {
+                    return evaluateExpression(args[i + 1]);
+                }
+            }
+            throw new Error('No true condition found in cond expression');
         default:
             throw new Error('Unknown operator: ' + operator);
     }
 }
 
 // Test
-console.log(evaluateExpression(['+', 1, 2])); // 输出 3
-console.log(evaluateExpression(['-', 5])); // 输出 -5
-console.log(evaluateExpression(['*', 2, 3])); // 输出 6
-console.log(evaluateExpression(['/', 10, 2])); // 输出 5
+console.log(evaluateExpression(['+', 1, 2])); // Output: 3
+console.log(evaluateExpression(['-', 5])); // Output: -5
+console.log(evaluateExpression(['*', 2, 3])); // Output: 6
+console.log(evaluateExpression(['/', 10, 2])); // Output: 5
+console.log(evaluateExpression(['cond', true, 1, false, 2, true, 3])); // Output: 1
