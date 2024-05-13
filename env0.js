7097e25b68d2f2eb5eac205b789926ed0bee6c4c
pica <xi_aochi@163.com>
2024-05-13 10:25:00 +0800

    运算符

--- env0.js
+++ env0.js
@@ -6,13 +6,28 @@ const env0 = {
     '=': (a, b) => a === b,
     '<': (a, b) => a < b,
     '>': (a, b) => a > b,
-    'abs': function (num) {
-        if (num < 0) {
-            return -num;
-        }
-        return num;
-    }
+    '%': (a, b) => a % b, // Modulus operator
+    '**': (a, b) => a ** b, // Exponentiation operator
+    '&&': (a, b) => a && b, // Logical AND
+    '||': (a, b) => a || b, // Logical OR
+    '!==': (a, b) => a !== b, // Not equal to
+    '<=': (a, b) => a <= b, // Less than or equal to
+    '>=': (a, b) => a >= b, // Greater than or equal to
+    '!=': (a, b) => a != b, // Not equal to (non-strict)
+    '<<': (a, b) => a << b, // Left shift
+    '>>': (a, b) => a >> b, // Right shift
+    '>>>': (a, b) => a >>> b, // Unsigned right shift
+    '&': (a, b) => a & b, // Bitwise AND
+    '|': (a, b) => a | b, // Bitwise OR
+    '^': (a, b) => a ^ b, // Bitwise XOR
+    '~': (a) => ~a, // Bitwise NOT (Unary)
+    'typeof': (a) => typeof a, // Typeof operator
+    'instanceof': (a, b) => a instanceof b, // Instanceof operator
+    'in': (a, b) => a in b, // In operator
+    'delete': (a, b) => delete a[b], // Delete operator
+    '.': (a, b) => a[b],
 };
+
 for (const name in env0) {
     const f = env0[name];
     const length = f.length;
